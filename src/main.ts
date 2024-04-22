ddb5ed455c4e02d11f5cac90a476c51a0f019214
Carl James Roxas <108602920+Just-a-NoobieDev@users.noreply.github.com>
2024-04-23 07:58:02 +0800

    TICKET-3 setup: add class-validator and logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PORT } from './config';
+import { NODE_ENV, PORT } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('VOICEOUT');
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
+
+  logger.log(
+    `Server running on http://localhost:${PORT}/graphql at ${NODE_ENV?.toUpperCase()} environment`,
+  );
 }
-bootstrap();
+
+try {
+  bootstrap();
+} catch (error) {
+  console.log(error);
+}
