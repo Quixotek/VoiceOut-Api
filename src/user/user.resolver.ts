7d6a321bed3130dcf3badae286ce25c082435500
Carl James Roxas <108602920+Just-a-NoobieDev@users.noreply.github.com>
2024-04-25 13:44:44 +0800

    fix: error in deployment

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -7,12 +7,12 @@ export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
   @Query(() => User)
-  async viewer(@Args() id: string) {
+  async viewer(@Args('id') id: string) {
     return await this.userService.findUserById(id); //TODO currentUser
   }
 
   @ResolveField(() => String)
-  async userId(@Args() id: string) {
+  async userId(@Args('id') id: string) {
     const user = await this.userService.findUserById(id); //TODO clean this, just testing ResolveField
 
     return user.id;
